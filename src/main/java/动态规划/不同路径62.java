c83142d796583d7cf9a31cd1ea9a96645cb51845
jishuai <jishuai@kanzhun.com>
2022-11-04 11:46:25 +0800

    update

---
+++ src/main/java/动态规划/不同路径62.java
@@ -0,0 +1,28 @@
+package 动态规划;
+
+public class 不同路径62 {
+    public int uniquePaths(int m, int n) {
+        if (m < 0 || n < 0) {
+            return 0;
+        }
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i > 0 && j > 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else if (i > 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else if (j > 0) {
+                    dp[i][j] = dp[i][j - 1];
+                }
+
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new 不同路径62().uniquePaths(6, 3));
+    }
+}
---
+++ src/main/java/动态规划/不同路径II63.java
@@ -0,0 +1,29 @@
+package 动态规划;
+
+public class 不同路径II63 {
+
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+
+        int[][] dp = new int[m][n];
+        dp[0][0] = obstacleGrid[0][0] == 1 ? 0 : 1;
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (obstacleGrid[i][j] == 1) {
+                    dp[i][j] = 0;
+                } else if (i > 0 && j > 0) {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+                } else if (i > 0) {
+                    dp[i][j] = dp[i - 1][j];
+                } else if (j > 0) {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+        return dp[m - 1][n - 1];
+    }
+
+}
