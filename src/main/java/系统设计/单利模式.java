8255adb77de0b71beb7b170973ad6f3968a777d8
jishuai <jishuai@kanzhun.com>
2023-03-23 20:53:55 +0800

    update

--- src/main/java/系统设计/单利模式.java
+++ src/main/java/系统设计/单利模式.java
@@ -20,4 +20,10 @@ public class 单利模式 {
         }
     }
 
+    public static void main(String[] args) {
+        Singleton s1 = Singleton.getInstance();
+        Singleton s2 = Singleton.getInstance();
+        System.out.println(s1 == s2);
+    }
+
 }
