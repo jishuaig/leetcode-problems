0da56c4528b4308a914024966ec6009c79e3c6cb
jishuai <jishuai@kanzhun.com>
2023-03-23 20:44:05 +0800

    update

--- src/main/java/树/平衡二叉树110.java
+++ src/main/java/树/平衡二叉树110.java
@@ -27,4 +27,15 @@ public class 平衡二叉树110 {
         return Math.abs(left - right) < 2 ? Math.max(left, right) + 1 : -1;
     }
 
+    // testIsBalanced
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(3);
+        root.left = new TreeNode(9);
+        root.right = new TreeNode(20);
+        root.right.left = new TreeNode(15);
+        root.right.right = new TreeNode(7);
+        System.out.println(new 平衡二叉树110().isBalanced(root));
+        System.out.println(new 平衡二叉树110().isBalanced2(root));
+    }
+
 }
